a192b05fff915dc33262fc73cd9a8bf129686db4
Felix <112383606+B9O2@users.noreply.github.com>
2026-06-08 18:50:16 +0800

    update

---
+++ neovim/.config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        go = { "golangcilint" },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "golangci-lint" })
+    end,
+  },
+}
