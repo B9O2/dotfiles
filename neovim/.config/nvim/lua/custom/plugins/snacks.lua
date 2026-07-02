9f598c606a476eadd673e87898a6647711ab5b32
Felix <112383606+B9O2@users.noreply.github.com>
2026-07-02 15:01:10 +0800

    rust

--- neovim/.config/nvim/lua/custom/plugins/snacks.lua
+++ neovim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -1,5 +1,5 @@
 return {
-  "folke/snacks.nvim",
+  'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
   opts = {
@@ -10,23 +10,131 @@ return {
     notifier = { enabled = true },
   },
   keys = {
-    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
-    { "<leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete Other Buffers" },
-    { "<leader>e", function() Snacks.explorer() end, desc = "Toggle Explorer" },
-    { "<leader><space>", function() Snacks.picker.buffers() end, desc = "[ ] Find existing buffers" },
-    { "<leader>sf", function() Snacks.picker.files() end, desc = "[S]earch [F]iles" },
-    { "<leader>sg", function() Snacks.picker.grep() end, desc = "[S]earch by [G]rep" },
-    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    { "<leader>nn", function() Snacks.notifier.show_history() end, desc = "Notification History" },
-    { "<leader>sh", function() Snacks.picker.help() end, desc = "[S]earch [H]elp" },
-    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "[S]earch [K]eymaps" },
-    { "<leader>sp", function() Snacks.picker.pickers() end, desc = "[S]earch [P]ickers" },
-    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "[S]earch [S]ymbols" },
-    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "[S]earch Workspace [S]ymbols" },
-    { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "[S]earch current [W]ord" },
-    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "[S]earch [D]iagnostics" },
-    { "<leader>sr", function() Snacks.picker.resume() end, desc = "[S]earch [R]esume" },
-    { "<leader>s.", function() Snacks.picker.recent() end, desc = '[S]earch Recent Files ("." for repeat)' },
-    { "<leader>sn", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "[S]earch [N]eovim files" },
+    {
+      '<leader>bd',
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = 'Delete Buffer',
+    },
+    {
+      '<leader>bo',
+      function()
+        Snacks.bufdelete.other()
+      end,
+      desc = 'Delete Other Buffers',
+    },
+    {
+      '<leader>e',
+      function()
+        Snacks.explorer()
+      end,
+      desc = 'Toggle Explorer',
+    },
+    {
+      '<leader><space>',
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = '[ ] Find existing buffers',
+    },
+    {
+      '<leader>sf',
+      function()
+        Snacks.picker.files()
+      end,
+      desc = '[S]earch [F]iles',
+    },
+    {
+      '<leader>sg',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = '[S]earch by [G]rep',
+    },
+    {
+      '<leader>un',
+      function()
+        Snacks.notifier.hide()
+      end,
+      desc = 'Dismiss All Notifications',
+    },
+    {
+      '<leader>n',
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = 'Notification History',
+    },
+    {
+      '<leader>sh',
+      function()
+        Snacks.picker.help()
+      end,
+      desc = '[S]earch [H]elp',
+    },
+    {
+      '<leader>sk',
+      function()
+        Snacks.picker.keymaps()
+      end,
+      desc = '[S]earch [K]eymaps',
+    },
+    {
+      '<leader>sp',
+      function()
+        Snacks.picker.pickers()
+      end,
+      desc = '[S]earch [P]ickers',
+    },
+    {
+      '<leader>ss',
+      function()
+        Snacks.picker.lsp_symbols()
+      end,
+      desc = '[S]earch [S]ymbols',
+    },
+    {
+      '<leader>sS',
+      function()
+        Snacks.picker.lsp_workspace_symbols()
+      end,
+      desc = '[S]earch Workspace [S]ymbols',
+    },
+    {
+      '<leader>sw',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = '[S]earch current [W]ord',
+    },
+    {
+      '<leader>sd',
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = '[S]earch [D]iagnostics',
+    },
+    {
+      '<leader>sr',
+      function()
+        Snacks.picker.resume()
+      end,
+      desc = '[S]earch [R]esume',
+    },
+    {
+      '<leader>s.',
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = '[S]earch Recent Files ("." for repeat)',
+    },
+    {
+      '<leader>sn',
+      function()
+        Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = '[S]earch [N]eovim files',
+    },
   },
 }
