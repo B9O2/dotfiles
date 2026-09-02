e2e98db99977eddbd3dbd32a7af8da4c5e166c95
Felix <112383606+B9O2@users.noreply.github.com>
2026-09-02 18:56:27 +0800

    set cwd before compile

--- neovim/.config/nvim/lua/custom/plugins/compile-mode.lua
+++ neovim/.config/nvim/lua/custom/plugins/compile-mode.lua
@@ -15,8 +15,30 @@ return {
       default_command = '', -- default to empty instead of 'make -k'
       focus_compilation_buffer = true, -- auto-focus output window
     }
+
+    local root_markers = {
+      '.git',
+      'go.mod',
+      'Cargo.toml',
+      'package.json',
+      'pyproject.toml',
+      'Makefile',
+      'CMakeLists.txt',
+      'compile_commands.json',
+    }
+
+    local function compile_root()
+      local bufname = vim.api.nvim_buf_get_name(0)
+      local start = bufname ~= '' and bufname or vim.fn.getcwd()
+      return vim.fs.root(start, root_markers) or vim.fn.getcwd()
+    end
+
+    vim.api.nvim_create_user_command('CompileProject', function()
+      vim.g.compilation_directory = compile_root()
+      vim.cmd.Compile()
+    end, {})
   end,
   keys = {
-    { '<leader>cc', '<cmd>Compile<cr>', desc = 'Compile (Emacs style)' },
+    { '<leader>cc', '<cmd>CompileProject<cr>', desc = 'Compile project' },
   },
 }
